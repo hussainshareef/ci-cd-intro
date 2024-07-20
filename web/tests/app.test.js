590ba7ab892e30143da1b6d59db048dbc18033ca
Mohammed Hussain Shareef <114469176+hussainshareef@users.noreply.github.com>
2024-07-21 05:15:02 +0530

    Update app.test.js

--- web/tests/app.test.js
+++ web/tests/app.test.js
@@ -1,7 +1,7 @@
 /**
  * A random test that does not fail
  **/
-describe('Sample Test', () => {
+describe('Sample Test 1', () => {
   it('should test that true === true', () => {
     expect(true).toBe(true)
   })
